76625984539dbf60bff2aaa7688c6a7ee62f0875
Fabio Henrique Chaves(Fabs) <fabio.hc94@gmail.com>
2024-12-28 01:44:09 -0300

    update sell operation

--- src/stocks/services/sell_operation.clj
+++ src/stocks/services/sell_operation.clj
@@ -12,25 +12,25 @@
   (execute [_ stocks-info profit operation]
     (let [unit-cost-total (input/get-total-cost operation (:unit-cost operation))
           avg-price-total (input/get-total-cost operation (:average-price stocks-info))
-          updated-stocks-info (stock/subtract-shares stocks-info operation)
-          updated-profit profit]
+          updated-stocks-info (stock/subtract-shares stocks-info operation)]
       (cond
         (= (:unit-cost operation) (:average-price updated-stocks-info))
         (do
-          (assoc updated-profit :gains math/ZERO)
-          [updated-stocks-info updated-profit])
-
+          (let [updated-profit (assoc profit :gains math/ZERO)] 
+            [updated-stocks-info updated-profit]))
+        
         (< (:unit-cost operation) (:average-price updated-stocks-info))
         (do
-          (profit/add-losses updated-profit (- avg-price-total unit-cost-total))
-          (assoc updated-profit :gains math/ZERO)
-          [updated-stocks-info updated-profit])
-
+          (let [updated-profit (profit/add-losses profit (- avg-price-total unit-cost-total))
+                updated-profit (assoc updated-profit :gains math/ZERO)]
+            [updated-stocks-info updated-profit]))
         :else
         (do
-          (assoc updated-profit :gains (- unit-cost-total avg-price-total))
-          (profit/process-losses updated-profit)
-          [updated-stocks-info updated-profit])))))
+          (let [updated-profit (assoc profit :gains (- unit-cost-total avg-price-total))
+                updated-profit (profit/process-losses updated-profit)]
+            [updated-stocks-info updated-profit]
+            ))))))
+
 
 (defn new-sell-operation []
   (->SellOperationImpl))

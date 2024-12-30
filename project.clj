c6635ac11295ee1479c69f35d2e5e8bcaa395e1f
Fabio Henrique Chaves(Fabs) <fabio.hc94@gmail.com>
2024-12-30 00:34:17 -0300

    Update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject stock-tax-calculation "0.1.0-SNAPSHOT"
   :description "Stocks tax calculation app"
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.4.0"]
                  [org.clojure/test.check "1.1.1"]]
   :repl-options {:init-ns stocks.core}
   :source-paths ["src"]

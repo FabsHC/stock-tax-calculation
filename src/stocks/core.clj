c8565a8671f2b93d47cfd2e5b5bfd259affd559c
Fabio Henrique Chaves(Fabs) <fabio.hc94@gmail.com>
2024-12-29 23:01:13 -0300

    add stdin read to core.clj

--- src/stocks/core.clj
+++ src/stocks/core.clj
@@ -1,21 +1,24 @@
 (ns stocks.core
-  (:require [stocks.models.stocks-info :as models]
-            [stocks.models.input :as input]
-            [stocks.models.profit :as profit]
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [stocks.handlers.tax-calculation :as tax]
             [stocks.services.buy-operation :as buy]
             [stocks.services.sell-operation :as sell]
-            [stocks.handlers.tax-calculation :as tax]))
+            ))
 
+(defn process-data
+  [operations]
+  (let [
+        buy-operation (buy/new-buy-operation)
+        sell-operation (sell/new-sell-operation)
+        tax-calculation (tax/new-tax-calculation buy-operation sell-operation)]
+    (tax/execute tax-calculation operations)
+    ))
 
-(def bo (buy/new-buy-operation))
-(def se (sell/new-sell-operation))
-(def tx (tax/new-tax-calculation bo se))
-
-(def operations
-  [{:operation input/BUY_OPERATION, :unit-cost 10.00, :quantity 10000}
-   {:operation input/SELL_OPERATION, :unit-cost 20.00, :quantity 5000}
-   {:operation input/SELL_OPERATION, :unit-cost 5.00, :quantity 5000}])
-
-(println "Input:\n" operations)
-(def result (tax/execute tx operations))
-(println "\nOutput:\n" result)
+(defn -main
+  [& args]
+  (let [input (slurp (io/reader *in*))
+        operations (json/read-str input :key-fn keyword)
+        result (process-data operations)
+        output (json/write-str result)]
+    (println output)))
